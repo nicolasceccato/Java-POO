be420c53c26c53603a63fb57ca208a0af7c8ca91
nicolasceccato <nicolasceccato@hotmail.com>
2022-09-13 13:12:48 -0300

    exercicio encapsulamento de um controle remoto Curso em Video

---
+++ encapsulamento/src/EncapsulamentoControle/Main.java
@@ -0,0 +1,14 @@
+package EncapsulamentoControle;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ControleRemoto c = new ControleRemoto();
+
+        c.ligar();
+        c.maisVolume();
+        c.maisVolume();
+        c.abrirMenu();
+
+    }
+}
