f61cc99bd818728ec9adc4a956e2076a6446d856
nicolasceccato <nicolasceccato@hotmail.com>
2023-06-10 14:22:08 -0300

    utilizando hibernate para criar registros em um banco de dados

---
+++ jpa/aula1/aulaJpa/src/main/java/aplicacao/Programa.java
@@ -0,0 +1,29 @@
+package aplicacao;
+
+import dominio.Pessoa;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Programa {
+    public static void main(String[] args) {
+
+
+        Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+        Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+        Pessoa p3 = new Pessoa(null, "Maria Chiquinha", "maria@gmail.com");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+        em.getTransaction().commit();
+        System.out.println("Pronto!");
+
+    }
+}
